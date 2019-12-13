6964b78f3782e37973a3b05d5041e4ebef2edf08
DIO0550 <diot12345@yahoo.co.jp>
2019-12-13 18:36:03 +0900

    対象OSをiOS12に設定。ビルドが通るように修正。

---
+++ src/DReversi/DReversi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DReversi
+//
+//  Created by DIO on 2019/12/13.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
