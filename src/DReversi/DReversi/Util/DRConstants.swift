0dbee6d6c4d29ad209f24904e20636f62fa3ddd2
DIO0550 <diot12345@yahoo.co.jp>
2019-12-15 21:44:53 +0900

    定数構造体ファイル新規追加

---
+++ src/DReversi/DReversi/Util/DRConstants.swift
@@ -0,0 +1,22 @@
+//
+//  DRConstants.swift
+//  DReversi
+//
+//  Created by DIO on 2019/12/14.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+struct DRConstants {
+    static let DRTitleShadowOffset: CGSize = CGSize(width: 3, height: 3)
+    static let DRTitleShadowBlurRadius: CGFloat = 3.0
+    static let DRTitleFontName: String = "SnellRoundhand-Bold"
+    static let DRTitleFontSizeRatio: CGFloat = 88.0 / 380.0
+    static let DRTitleAnimationRelativeDuration = 0.5
+    static let DRTitleAnimationFirstRelativeStartTime = 0.0
+    static let DRTitleAnimationSecondRelativeStartTime = 0.1
+    static let DRTitleAnimationThirdRelativeStartTime = 0.3
+    static let DRTitleAnimationFourthRelativeStartTime = 0.5
+    static let DRTitleAnimationeDuration = DRTitleAnimationRelativeDuration + DRTitleAnimationRelativeDuration
+}
