6ba81f02a05f2befdd9fdfa2e4da976350c65a86
DIO0550 <diot12345@yahoo.co.jp>
2019-12-15 21:39:12 +0900

    StringのExtension用ファイル追加

---
+++ src/DReversi/DReversi/Extension/String+Addtional.swift
@@ -0,0 +1,21 @@
+//
+//  String+Addtional.swift
+//  DReversi
+//
+//  Created by DIO on 2019/12/14.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    public func stringAtIndex(index: Int) -> String? {
+        if self.isIndexOutOfRange(index: index) { return nil }
+        let subString: Substring = self[self.index(self.startIndex, offsetBy: index) ..< self.index(self.startIndex, offsetBy: index + 1)]
+        return String(subString)
+    }
+    
+    private func isIndexOutOfRange(index: Int) -> Bool {
+        return (0 > index || index >= self.count)
+    }
+}
