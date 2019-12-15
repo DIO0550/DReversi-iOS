7249a5c613c369795ba494a40e0c48d75e81bcae
DIO0550 <diot12345@yahoo.co.jp>
2019-12-15 21:40:20 +0900

    ゲーム画面用のビューコントローラーをクラスを追加

---
+++ src/DReversi/DReversi/Game/DRGameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DRGameViewController.swift
+//  DReversi
+//  ゲーム画面
+//  Created by DIO on 2019/12/13.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+class DRGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
