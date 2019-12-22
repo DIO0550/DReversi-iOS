eb1d379b34387f28e3eeeedef8e4f7827e7b6faf
DIO0550 <diot12345@yahoo.co.jp>
2019-12-22 23:01:11 +0900

    定数用のファイル追加

---
+++ src/DReversiControl/DReversiControl/DReversiControlConst.swift
@@ -0,0 +1,13 @@
+//
+//  DReversiControlConst.swift
+//  DReversiControl
+//
+//  Created by DIO on 2019/12/22.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+public struct DReversiControlConst {
+    public static let BlockCount: Int = 8
+}
