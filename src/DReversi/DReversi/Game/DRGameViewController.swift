11f8142e248079bedb4dd8b587b18557d17cb98b
DIO0550 <diot12345@yahoo.co.jp>
2019-12-22 23:01:38 +0900

    初期配置の石を置く処理を追加

--- src/DReversi/DReversi/Game/DRGameViewController.swift
+++ src/DReversi/DReversi/Game/DRGameViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
-import DRevesiControl
+import DReversiControl
 
 class DRGameViewController: UIViewController {
+    
+    // MARK: Public Instance
     public var gameLevel: GameLevel = .NORMAL
-
+    public var playerStone: DRStoneType = .BLACK_STONE
+    
+    // MARK: Private Instance
+    private var gameManager: DRGameManager = DRGameManager()
+    private var isInitializeStone: Bool = false
+    
+    // MARK: IBOutlet Instance
     @IBOutlet weak var boardView: DRBoardView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        DispatchQueue.main.asyncAfter(deadline: .now()) { [weak self] in
+            guard let weakSelf = self else { return }
+            weakSelf.initializeStones()
+        }
     }
+    @IBAction func touchPutButton(_ sender: Any) {
+        let selectPosition = self.boardView.selectStonePosition
+        
+        self.gameManager.addStone(stonePosition: selectPosition,
+                                  boardView: self.boardView,
+                                  stoneType: self.playerStone)
+        
+        self.gameManager.reverseStone(putPosition: selectPosition, stoneType: self.playerStone)
+    }
+}
 
+extension DRGameViewController {
+    private func initializeStones() {
+        if self.isInitializeStone { return }
+        
+        self.isInitializeStone = true;
+        self.gameManager.addStone(stonePosition: DRStonePosition(column: 3, row: 3),
+                                  boardView: self.boardView,
+                                  stoneType: .WHITE_STONE)
+        self.gameManager.addStone(stonePosition: DRStonePosition(column: 4, row: 3),
+                                  boardView: self.boardView,
+                                  stoneType: .BLACK_STONE)
+        self.gameManager.addStone(stonePosition: DRStonePosition(column: 4, row: 4),
+                                  boardView: self.boardView,
+                                  stoneType: .WHITE_STONE)
+        self.gameManager.addStone(stonePosition: DRStonePosition(column: 3, row: 4),
+                                  boardView: self.boardView,
+                                  stoneType: .BLACK_STONE)
+        
+    }
 }
