2b1b59e0aad03964595b1d06afda5246c155dbdc
DIO0550 <diot12345@yahoo.co.jp>
2019-12-22 23:00:12 +0900

    ゲーム中のメニュー用ビュー追加

---
+++ src/DReversiControl/DReversiControl/UIView/DRMenuView.swift
@@ -0,0 +1,13 @@
+//
+//  DRMenuView.swift
+//  DReversiControl
+//
+//  Created by DIO on 2019/12/22.
+//  Copyright © 2019 DIO. All rights reserved.
+//
+
+import UIKit
+
+public class DRMenuView: UIView {
+
+}
