b3ba65dc7b8cab1fbdfeb0354d209e7001add3d5
DIO0550 <diot12345@yahoo.co.jp>
2020-02-14 18:55:10 +0900

    COMが石を置く処理などを追加

--- src/DReversi/DReversi/Game/DRGameViewController.swift
+++ src/DReversi/DReversi/Game/DRGameViewController.swift
@@ -14,22 +14,37 @@ class DRGameViewController: UIViewController {
     // MARK: Public Instance
     public var gameLevel: GameLevel = .NORMAL
     public var playerStone: DRStoneType = .BLACK_STONE
+    public var comStone: DRStoneType = .WHITE_STONE
     
     // MARK: Private Instance
     private var gameManager: DRGameManager = DRGameManager()
     private var isInitializeStone: Bool = false
+    private var gameAI: DRGameAIBase = DRGameAIEasy()
+    private var gameTurn: DRGameTurn = .PLAYER
     
     // MARK: IBOutlet Instance
     @IBOutlet weak var boardView: DRBoardView!
+    @IBOutlet weak var putStoneButton: UIButton!
+    @IBOutlet weak var blackStoneCountLabel: UILabel!
+    @IBOutlet weak var whiteStoneCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.boardView.delegate = self
+    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         DispatchQueue.main.asyncAfter(deadline: .now()) { [weak self] in
             guard let weakSelf = self else { return }
             weakSelf.initializeStones()
+            weakSelf.computerPutStoneLoop()
         }
     }
+    
     @IBAction func touchPutButton(_ sender: Any) {
+        if !self.gameTurn.isTurnPlayer() { return }
+        
         let selectPosition = self.boardView.selectStonePosition
         
         self.gameManager.addStone(stonePosition: selectPosition,
@@ -37,6 +52,11 @@ class DRGameViewController: UIViewController {
                                   stoneType: self.playerStone)
         
         self.gameManager.reverseStone(putPosition: selectPosition, stoneType: self.playerStone)
+        self.updatePutStoneButtonEnable()
+        
+        if self.canPutComStone() {
+            self.gameTurn = .COM
+        }
     }
 }
 
@@ -58,5 +78,64 @@ extension DRGameViewController {
                                   boardView: self.boardView,
                                   stoneType: .BLACK_STONE)
         
+    }
+
+    private func updatePutStoneButtonEnable() {
+        let isEnabled = self.gameManager.canPutReversePosition(stonePosition: self.boardView.selectStonePosition, stoneType: self.playerStone) && self.gameTurn.isTurnPlayer()
+        self.putStoneButton.isEnabled = isEnabled
+        self.putStoneButton.alpha = isEnabled ? 1.0 : 0.5
+    }
+    
+    private func canPutPlayerStone() -> Bool {
+        return self.gameManager.canPutStonePositions(stoneType: self.playerStone).count > 0
+    }
+    
+    private func canPutComStone() -> Bool {
+        return self.gameManager.canPutStonePositions(stoneType: self.comStone).count > 0
+    }
+    
+    private func comPutStone() {
+        if !self.canPutPlayerStone() { return }
+        let stoneReverseInfos = self.gameManager.stoneReverseInfos(stoneType: self.comStone)
+        let aiPutPosition = self.gameAI.putStonePosition(stoneReverseInfos: stoneReverseInfos)
+        self.gameManager.addStone(stonePosition: aiPutPosition,
+                                  boardView: self.boardView,
+                                  stoneType: self.comStone)
+        self.gameManager.reverseStone(putPosition: aiPutPosition, stoneType: self.comStone)
+        if self.canPutPlayerStone() {
+            self.gameTurn = .PLAYER
+        }
+        self.updatePutStoneButtonEnable()
+    }
+    
+    private func computerPutStoneLoop() {
+        DispatchQueue.global().async {
+            while true {
+                if self.gameTurn.isTurnPlayer() { continue }
+                Thread.sleep(forTimeInterval: 2.0)
+                let semaphore = DispatchSemaphore(value: 0)
+                DispatchQueue.main.sync { [weak self] in
+                    guard let weakSelf = self else {
+                        semaphore.signal()
+                        return
+                    }
+                    if weakSelf.gameTurn.isTurnPlayer() {
+                        semaphore.signal()
+                        return
+                    }
+                    weakSelf.comPutStone()
+                    semaphore.signal()
+                }
+                semaphore.wait()
+            }
+        }
+    }
+}
+
+extension DRGameViewController: DRBoardViewDelegate {
+    func boardView(_ boardView: DRBoardView, didSelectPosition position: DRStonePosition) {
+        let isEnabled = self.gameManager.canPutReversePosition(stonePosition: position, stoneType: self.playerStone) && self.gameTurn.isTurnPlayer()
+        self.putStoneButton.isEnabled = isEnabled
+        self.putStoneButton.alpha = isEnabled ? 1.0 : 0.5
     }
 }
