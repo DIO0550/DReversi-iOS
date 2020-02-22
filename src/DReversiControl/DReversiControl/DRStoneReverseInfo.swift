6fc9d491a0d49fc31132c6094470d96f20e7b4de
DIO0550 <diot12345@yahoo.co.jp>
2020-02-22 17:11:43 +0900

    Comparableに準拠するように変更

--- src/DReversiControl/DReversiControl/DRStoneReverseInfo.swift
+++ src/DReversiControl/DReversiControl/DRStoneReverseInfo.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-public struct DRStoneReverseInfo {
+public struct DRStoneReverseInfo: Comparable {
+ 
     public var stonePosition: DRStonePosition
     public var reverseCount: Int
     
     public init(stonePosition: DRStonePosition, reverseCount: Int) {
         self.stonePosition = stonePosition;
         self.reverseCount = reverseCount
+    }
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.reverseCount == rhs.reverseCount
+    }
+    
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.reverseCount < rhs.reverseCount
     }
 }
