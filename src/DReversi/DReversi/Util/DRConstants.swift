2d1f79067271bf8ecb3cbe07ce676e3aec33a935
DIO0550 <diot12345@yahoo.co.jp>
2020-03-07 21:58:03 +0900

    ゲーム難易度の記載場所を移動

--- src/DReversi/DReversi/Util/DRConstants.swift
+++ src/DReversi/DReversi/Util/DRConstants.swift
@@ -8,11 +8,6 @@
 
 import UIKit
 
-enum GameLevel {
-    case EASY
-    case NORMAL
-    case HARD
-}
 
 struct DRConstants {
     static let DRTitleShadowOffset: CGSize = CGSize(width: 3, height: 3)
