f4f94ee93fca66cb35c534538becf601fbb211aa
DIO0550 <diot12345@yahoo.co.jp>
2020-03-07 22:00:55 +0900

    通知時にレベルを設定するためのキーの定数を追加。

--- src/DReversiControl/DReversiControl/DReversiControlConst.swift
+++ src/DReversiControl/DReversiControl/DReversiControlConst.swift
@@ -12,4 +12,5 @@ public struct DReversiControlConst {
     public static let BlockCount: Int = 8
     public static let BoardCornerPosition: [DRStonePosition] = [DRStonePosition(column: 0, row: 0), DRStonePosition(column: 7, row: 0),
                                                                 DRStonePosition(column: 0, row: 7), DRStonePosition(column: 7, row: 7)]
+    public static let SelectLevelButtonKey = "DRSelectLevelButtonKey"
 }
