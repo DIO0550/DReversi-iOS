6e2958e18f419334c21da72dafff49027e5a81f9
DIO0550 <diot12345@yahoo.co.jp>
2020-03-07 21:51:30 +0900

    定数ファイル追加

---
+++ src/DReversiUtil/DReversiUtil/DReversiUtilConst.swift
@@ -0,0 +1,17 @@
+//
+//  DReversiUtilConst.swift
+//  DReversiUtil
+//
+//  Created by DIO on 2020/03/07.
+//  Copyright © 2020 DIO. All rights reserved.
+//
+
+
+
+public struct DReversiUtilConst {
+    public enum GameLevel {
+        case EASY
+        case NORMAL
+        case HARD
+    }
+}
