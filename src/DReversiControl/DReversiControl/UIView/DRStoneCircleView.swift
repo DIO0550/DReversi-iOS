1cc14bcea0e1958413b63925cdbb9bcc49f8a5f9
DIO0550 <diot12345@yahoo.co.jp>
2020-03-08 20:55:58 +0900

    frame更新時に、丸みの半径を調整する処理を追加。 #1

--- src/DReversiControl/DReversiControl/UIView/DRStoneCircleView.swift
+++ src/DReversiControl/DReversiControl/UIView/DRStoneCircleView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 
 public class DRStoneCircleView: UIView {
+    override public var frame: CGRect {
+        didSet {
+            let radius: CGFloat = self.frame.width > self.frame.height ? self.frame.width / 2.0 : self.frame.height / 2.0
+            self.layer.cornerRadius = radius
+        }
+    }
+    
     public init(frame: CGRect, color: UIColor) {
         super.init(frame: frame)
         let radius: CGFloat = frame.width > frame.height ? frame.width / 2.0 : frame.height / 2.0
