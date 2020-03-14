4bfae877094600f756cc5b52e5c2c661a36d17ae
DIO0550 <diot12345@yahoo.co.jp>
2020-03-14 22:10:49 +0900

    ゲーム終了時用の画面を追加。

---
+++ src/DReversi/DReversi/Game/DRGameResultView.swift
@@ -0,0 +1,21 @@
+//
+//  DRGameResultView.swift
+//  DReversi
+//
+//  Created by DIO on 2020/03/14.
+//  Copyright © 2020 DIO. All rights reserved.
+//
+
+import UIKit
+
+class DRGameResultView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
