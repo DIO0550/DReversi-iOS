fdd07025a904191cf450ff0cc029e2e3a0a1fff9
DIO0550 <diot12345@yahoo.co.jp>
2020-03-14 22:11:47 +0900

    nibから初期化する処理を追加。

--- src/DReversi/DReversi/Game/DRGameResultView.swift
+++ src/DReversi/DReversi/Game/DRGameResultView.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class DRGameResultView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+        self.backgroundColor = .clear
+        let nib = UINib.init(nibName: String(describing: type(of: self)), bundle: Bundle.main)
+        let subView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        subView.frame = CGRect(origin: CGPoint.zero, size: self.frame.size)
+        self.addSubview(subView)
+        
+        let views = ["subView": subView]
+        
+        let vVisualFormat = "V:|-0-[subView]-0-|"
+        let vConstraints = NSLayoutConstraint.constraints(withVisualFormat: vVisualFormat, options:.alignAllTop, metrics: nil, views: views)
+        
+        let hVisualFormat = "H:|-0-[subView]-0-|"
+        let hConstraints = NSLayoutConstraint.constraints(withVisualFormat: hVisualFormat, options:.alignAllTop, metrics: nil, views: views)
+        
+        self.addConstraints(vConstraints)
+        self.addConstraints(vConstraints)
+    }
 }
