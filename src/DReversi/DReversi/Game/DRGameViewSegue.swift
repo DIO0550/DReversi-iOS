42a0adca6474cd7ebb15d5813a27df192e382471
DIO0550 <diot12345@yahoo.co.jp>
2020-03-17 00:04:23 +0900

    ゲーム画面から戻る処理を追加。

--- src/DReversi/DReversi/Game/DRGameViewSegue.swift
+++ src/DReversi/DReversi/Game/DRGameViewSegue.swift
@@ -15,12 +15,18 @@ class DRGameViewSegue: UIStoryboardSegue {
         
         if destVC is DRGameViewController && sourceVC is DRTitleViewController {
             (destVC as! DRGameViewController).gameLevel = (sourceVC as! DRTitleViewController).gameLevel
-        }
-        
-        UIView.transition(with: source.view, duration: 0.5, options: .transitionFlipFromTop, animations: {
-            sourceVC.view.addSubview(destVC.view)
-        }) { finish in
-            sourceVC.dismiss(animated: false, completion:nil)
+            
+            UIView.transition(with: sourceVC.navigationController!.view, duration: 0.5, options: .transitionFlipFromTop, animations: {
+                sourceVC.navigationController!.pushViewController(destVC, animated: false)
+            }) { finish in
+                
+            }
+        } else {
+            UIView.transition(with: sourceVC.navigationController!.view, duration: 0.5, options: .transitionFlipFromTop, animations: {
+                sourceVC.navigationController!.popViewController(animated: false)
+            }) { finish in
+                
+            }
         }
     }
 }
