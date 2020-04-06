d0e8b718363fb607a9c906f527fbebb5a07f7e6d
DIO0550 <diot12345@yahoo.co.jp>
2020-04-06 22:51:46 +0900

    石を描画する処理まで追加

--- src/DRImageGenerator/DRImageGenerator/View/DRImageGeneratorCircleView.swift
+++ src/DRImageGenerator/DRImageGenerator/View/DRImageGeneratorCircleView.swift
@@ -10,10 +10,25 @@ import Cocoa
 
 class DRImageGeneratorCircleView: NSView {
 
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-
-        // Drawing code here.
+    override public var frame: CGRect {
+        didSet {
+            let radius: CGFloat = self.frame.width > self.frame.height ? self.frame.width / 2.0 : self.frame.height / 2.0
+            self.layer?.cornerRadius = radius
+        }
     }
     
+    public init(frame: CGRect, color: NSColor) {
+        super.init(frame: frame)
+        self.wantsLayer = true
+        let radius: CGFloat = frame.width > frame.height ? frame.width / 2.0 : frame.height / 2.0
+        self.layer?.cornerRadius = radius
+        self.layer?.backgroundColor = color.cgColor
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.wantsLayer = true
+        let radius: CGFloat = frame.width > frame.height ? frame.width / 2.0 : frame.height / 2.0
+        self.layer?.cornerRadius = radius
+    }
 }
